9df7f4beb7b1ee212feb61ae3c57ad3918005590
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-05-07 16:55:06 +0700

    Create CategoryRepository.java

---
+++ Spring_JPA/src/main/java/com/example/Spring_JPA/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.example.Spring_JPA.repository;
+
+import com.example.Spring_JPA.entity.Category;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
